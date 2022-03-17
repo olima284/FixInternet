37e94d305e4ee12e88bbd3f1fa6f721dec046c52
olima284 <olivialima.student@careerdevs.com>
2022-03-17 16:25:57 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/FixInternet/FixInternetApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.FixInternet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixInternetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/FixInternet/FixInternetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.FixInternet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixInternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
